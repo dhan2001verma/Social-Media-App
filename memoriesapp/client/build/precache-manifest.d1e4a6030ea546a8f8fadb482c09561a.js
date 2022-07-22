b895f4d4022c77b7378dbb783603a4ce31b13c03
Dhanraj verma <73555435+dhan2001verma@users.noreply.github.com>
2022-07-22 20:34:24 +0530

    Add files via upload

---
+++ memoriesapp/client/build/precache-manifest.d1e4a6030ea546a8f8fadb482c09561a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a641f064a310204b96eb7ab61184d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9110d511b5c93a75a240",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "d474f61b3002f42dae90",
+    "url": "/static/js/2.8f1998e3.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.8f1998e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9110d511b5c93a75a240",
+    "url": "/static/js/main.e7594c98.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
---
+++ memoriesapp/server/index.js
@@ -0,0 +1,28 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/',(req,res)=>{
+  res.send('HELLO TO MY APP');
+});
+
+const CONNECTION_URL='mongodb+srv://dhannu2001:Dhan2001verma@cluster0.4schc.mongodb.net/fff?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
